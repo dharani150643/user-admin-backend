67305aecc2a589f94f75fafb0aad626fa06833cd
dharani150643 <dharanidharyadav01@gmail.com>
2025-06-24 22:10:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: 'sql12.freesqldatabase.com',
-  user: 'sql12786610',
-  password: 'aVddBY8LgV',
-  database: 'sql12786610'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 connection.connect(err => {
-  if (err) throw err;
+  if (err) {
+    console.error('Database connection failed:', err);
+    return;
+  }
   console.log('MySQL Connected!');
 });
 module.exports = connection;
